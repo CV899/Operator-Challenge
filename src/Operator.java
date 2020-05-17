73e689b48f60a1475e97208dabed5d17bbf9df69
Christian V <christian.vincent25@hotmail.com>
2020-05-17 14:36:00 -0400

    Final Commit

--- src/Operator.java
+++ src/Operator.java
@@ -2,5 +2,19 @@ public class Operator {
 
     public static void main(String[] args) {
 
+        double myFirstDouble = 20.00d;
+        double mySecondDouble = 80.00d;
+        double result = (myFirstDouble + mySecondDouble) * 100.00d;
+        double remainder = result % 40.00d;
+
+        boolean isZero = (remainder == 0) ? true : false;
+
+        System.out.println(isZero);
+
+        if(!isZero) {
+            System.out.println("Got some remainder");
+        }
+
+
     }
 }
