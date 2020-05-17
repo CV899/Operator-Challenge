f2319c01c944d822654237fafb9e0177fcd3bd7d
Christian V <christian.vincent25@hotmail.com>
2020-05-17 14:45:13 -0400

    Final Commit

--- src/Operator.java
+++ src/Operator.java
@@ -4,8 +4,8 @@ public class Operator {
 
         double myFirstDouble = 20.00d;
         double mySecondDouble = 80.00d;
-        double result = (myFirstDouble + mySecondDouble) * 100.00d;
-        double remainder = result % 40.00d;
+        double result = (myFirstDouble + mySecondDouble) * 100.00d;     // result = 10000
+        double remainder = result % 40.00d;                             // (10000 / 40) = 250 remainder 0
 
         boolean isZero = (remainder == 0) ? true : false;
 
